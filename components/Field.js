115f999b4c7a0d25339a29c54bff6de4a136a6c0
Luke Plaster <me@lukep.org>
2017-10-25 16:01:51 +0800

    Started on the create form

---
+++ components/Field.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { Box, Text } from 'grommet';
+
+const Field = ({
+  children,
+  error,
+  label,
+  help,
+}) => {
+  let header;
+  if (label || help || error) {
+    header = (
+      <Box
+        direction='row'
+        justify='between'
+        pad={{ horizontal: 'small', top: 'xsmall' }}
+      >
+        <Text>{label}</Text>
+        <Text color={error ? 'status-critical' : 'dark-5'}>{error || help}</Text>
+      </Box>
+    );
+  }
+  return (
+    <Box
+      direction='column'
+      border={{ color: 'light-2', side: 'bottom', size: 'small' }}
+      margin={{ vertical: 'xsmall' }}
+    >
+      {header}
+      {children}
+    </Box>
+  );
+};
+
+export default Field;
--- pages/create.js
+++ pages/create.js
@@ -1,9 +1,59 @@
 import React from 'react';
 
+import { Box, Heading, Button, Paragraph, TextInput, CheckBox } from 'grommet';
+import styled from 'styled-components';
+
+import { WidthCappedContainer } from '../components/WidthCappedContainer';
 import Layout from '../components/layout';
+import Field from '../components/Field';
+
+const BorderlessTextarea = styled.textarea`
+  border: 0;
+  font-size: 16px;
+  padding: 16px;
+`;
 
 export default () => (
   <Layout>
-    <div>Hello World.</div>
+    <WidthCappedContainer>
+      <form
+        name='create-form'
+        onSubmit={(ev) => {
+          ev.preventDefault();
+          return false;
+        }}
+      >
+        <Box>
+          <Heading level={2} margin='none'>
+            Create a new paste
+          </Heading>
+          <Box margin={{ vertical: 'medium', horizontal: 'none', bottom: 'large' }}>
+            <Paragraph margin={{ bottom: 'medium' }}>
+              Create a new pastebin entry. <br />
+              Please enter your text and choose a fitting title.
+            </Paragraph>
+            <Field label='Title'>
+              <TextInput plain={true} />
+            </Field>
+            <Field label='Content'>
+              <BorderlessTextarea placeholder='Enter some text' />
+            </Field>
+            <Field>
+              <Box pad='small' margin={{ bottom: 'small' }}>
+                <CheckBox
+                  label='A checkbox'
+                  onChange={() => {
+                    return null;
+                  }}
+                />
+              </Box>
+            </Field>
+            <Box margin={{ top: 'medium' }}>
+              <Button primary={true} type='submit' label='Submit' />
+            </Box>
+          </Box>
+        </Box>
+      </form>
+    </WidthCappedContainer>
   </Layout>
 );
